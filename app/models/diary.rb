21621b9bc4e490d107f19e5e1dc27e859d5062df
pedram aghasian <pedram.aghasian@yahoo.com>
2020-03-02 05:23:58 +0800

    change tempate

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,5 +1,5 @@
 class Diary < ApplicationRecord
-  validates :name , presence: true
+  validates :name , presence: true , length: {maximum:30}
   validates :description , presence: true, length: {minimum:5,maximum:500}
   validates :user_id , presence: true
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root "pages#home"
   get "pages/home", to: "pages#home"
 
+  get "pages/about",to: "pages#about"
+
+  get "pages/contact",to: "pages#contact"
+
   resources :diaries
 end
